daac25e8262dac212b1cc167d4453218a3aaa28a
Daniel Boman <daniel.boman@pm.me>
2024-09-03 20:44:29 +0200

    Updates

--- flake.nix
+++ flake.nix
@@ -75,55 +75,6 @@
         NSGlobalDomain.InitialKeyRepeat = 10;
         NSGlobalDomain.KeyRepeat = 4;
       };
-
-      services.yabai = {
-      	enable = false;
-      	enableScriptingAddition = true;
-   		config = {
-   		  focus_follows_mouse          = "autoraise";
-   		  mouse_follows_focus          = "off";
-   		  window_placement             = "second_child";
-   		  window_opacity               = "off";
-   		  window_opacity_duration      = "0.0";
-   		  window_border                = "on";
-   		  window_border_placement      = "inset";
-   		  window_border_width          = 2;
-   		  window_border_radius         = 3;
-   		  active_window_border_topmost = "off";
-   		  window_topmost               = "on";
-   		  window_shadow                = "float";
-   		  active_window_border_color   = "0xff5c7e81";
-   		  normal_window_border_color   = "0xff505050";
-   		  insert_window_border_color   = "0xffd75f5f";
-   		  active_window_opacity        = "1.0";
-   		  normal_window_opacity        = "1.0";
-   		  split_ratio                  = "0.50";
-   		  auto_balance                 = "on";
-   		  mouse_modifier               = "fn";
-   		  mouse_action1                = "move";
-   		  mouse_action2                = "resize";
-   		  layout                       = "bsp";
-   		  top_padding                  = 36;
-   		  bottom_padding               = 10;
-   		  left_padding                 = 10;
-   		  right_padding                = 10;
-   		  window_gap                   = 10;
-   		};
-      };
-    services.skhd = {
-    	enable = true;
-    	skhdConfig = ''
-    		ctrl + alt - left : yabai -m window --focus west
-    		ctrl + alt - right : yabai -m window --focus east
-    		ctrl + alt - down : yabai -m window --focus south
-    		ctrl + alt - up : yabai -m window --focus north
-    		ctrl + alt - v : yabai -m window --toggle float --grid 4:4:1:1:2:2
-    		ctrl + alt - t : alacritty
-    		
-    		
-    	'';
-    };
-
       
     };
     homeconfig = {pkgs, ...}: {
@@ -177,7 +128,6 @@
               "golang"
               "rust"
               "docker"
-              #"zsh-autosuggestions"
               "ssh-agent"
             ];
             theme = "af-magic";
@@ -190,7 +140,6 @@
         enable = true;
         settings = {
           window = {
-            #decorations = "Transparent";
             blur = true;
           };
           font = {
